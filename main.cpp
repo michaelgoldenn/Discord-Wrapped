3e7d12822f60352337790875b1eca177f15d3b33
ycoronam <159861009+ycoronam@users.noreply.github.com>
2024-03-24 20:12:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <xlsxwriter.h>
 
 int main(){
     //just hello world, remove this later
     std::cout << "Hello World";
+
+
+    
+    /* Create a new workbook and add a worksheet. */
+    lxw_workbook  *workbook  = workbook_new("example.xlsx");
+    lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
+
+    // Write some data to cells
+    worksheet_write_string(worksheet, 0, 0, "Hello", NULL);
+    worksheet_write_number(worksheet, 0, 1, 123, NULL);
+    worksheet_write_number(worksheet, 1, 0, 456, NULL);
+
+    workbook_close(workbook);
     return 0;
 }

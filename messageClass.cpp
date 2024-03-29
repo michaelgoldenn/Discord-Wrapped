2cdb24822adf134147fdcb18592ac617c60339ef
michaelgoldenn <michael.golden0278@gmail.com>
2024-03-29 12:15:57 -0400

    Restructured and wrote some functions

--- messageClass.cpp
+++ messageClass.cpp
@@ -7,89 +7,87 @@
 class Message {
     public:
         time_t timeStamp;
-        std::string content;
+        string content;
 
         int pingCount;
         int wordCount;
         bool isReply;
-        bool isCallEnd;
+        time_t callTime; //If the message was a call, how long was it? If it isn't a call, should be -1
         bool containsLink;
 
-        //Default constructor sets everything to either 0 or false
-        Message(){
-            pingCount = 0;
-            wordCount = 0;
-            isReply = false;
-            isCallEnd = false;
-            containsLink = false;
+        //Message Constructor
+        Message(time_t time, string message){
+            timeStamp = time;
+            content = message;
+            //fill in the other variables from the message
+            callTime = getCallTime();
+            pingCount = getPingCount();
+            wordCount = getWordCount();
+            isReply = getIsReply();
+            containsLink = getContainsLink();
         }
 
-        //Message Constructor with parameters
-        Message(int pCount, int wCount, bool reply, bool callEnd, bool link){
-            pingCount = pCount;
-            wordCount = wCount;
-            isReply = reply;
-            isCallEnd = callEnd;
-            containsLink = link;
-        }
-
-        //a bunch of setter methods
-        void setContent(std::string newContent){
-            content = newContent;
-        }
-
-        void setTimeStamp(time_t newTimeStamp){
-            timeStamp = newTimeStamp;
-        }
-
-        void setPingCount(int newPingCount){
-            pingCount = newPingCount;
-        }
-
-        void setWordCount(int newWordCount){
-            wordCount = newWordCount;
-        }
-
-        void setIsReply(bool newIsReply){
-            isReply = newIsReply;
-        }
-
-        void setIsCallEnd(bool newIsCallEnd){
-            isCallEnd = newIsCallEnd;
-        }
-
-        void setContainsLink(bool newContainsLink){
-            containsLink = newContainsLink;
-        }
-
-
-        //a bunch of getter methods
-        std::string getContent(){
-            return content;
-        }
-
-        time_t getTimeStamp(){
-            return timeStamp;
-        }
-
+    private:
+        //Gets pingCount from the content
         int getPingCount(){
-            return pingCount;
+            if (str == NULL)
+                return -1;
+            //AWFUL implementation - just counts the nubmer of times a message has @. *please* change this later
+            int count = 0;
+            for (int i=0; i<content.size(); i++){
+                if (content[i] == "@"){
+                    count++;
+                }
+            }
+            return count;
         }
 
+
+        //Gets word from the content
         int getWordCount(){
-            return wordCount;
+            if (str == NULL)
+                return -1;
+
+            bool inSpaces = true;
+            int numWords = 0;
+
+            while (*str != '\0')
+            {
+                if (std::isspace(*str)){
+                    inSpaces = true;
+                } else if (inSpaces){
+                    numWords++;
+                    inSpaces = false;
+                }
+
+                ++str;
+            }
+
+            return numWords;
         }
 
+
+        //Gets if it is a reply from the content
         bool getIsReply(){
+            //waiting till html things are figured out for this implementation
             return isReply;
         }
 
-        bool getIsCallEnd(){
-            return isCallEnd;
+
+        //Gets if it is a message indicating the end of a call
+        time_t getCallTime(){
+            //waiting till html things are figured out for this implementation
+            return 0;
         }
 
+
+        //Gets if the message contains a link
+        //not great but should work
         bool getContainsLink(){
-            return containsLink;
+            if (std::regex_match ("http://subject", std::regex("^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$") )) {
+                return true;
+            }
+            return false;
         }
 };
 
